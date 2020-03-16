82a804ec89db9338b3928c0a8d20f56f2283ff29
utente <utente@debian10.units.it>
2020-03-16 11:06:02 +0100

    Esercizio assegnato il 13-03-2020

---
+++ semplice_operazione/semplice_operazione.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/*
+ * semplice_operazione.c
+ *
+ *  Created on: 15 mar 2020
+ *      Author: utente
+ */
+int main(int argc, char**argv){
+	float a;
+	float b;
+	char op;
+	short test;
+	float res;
+	printf("Scrivi l'operazione da svolgere nella sequenza op, num, num:\n");
+	test=scanf("%c %f %f", &op, &a, &b);
+	//scanf("%*s");
+	if(test==3){
+		if(op=='+'){
+			res=a+b;
+			printf("Il risultato è: %.3f", res);
+			printf("caio mondon");
+			return 0;
+		}
+		if(op=='-'){
+			res=a-b;
+			printf("Il risultato è: %.3f", res);
+			return 0;
+		}
+		if(op=='*'){
+			res=a*b;
+			printf("Il risultato è: %.3f", res);
+			return 0;
+		}
+		if(op=='/' && b!=0){
+			res=a/b;
+			printf("Il risultato è: %.3f", res);
+			return 0;
+		}
+		else{
+			printf("Non puoi dividere per 0, ciao!");
+		}
+		printf("L'operazione non è valida, ciao!");
+		return -1;
+	}
+	else{
+		printf("Mi hai dato dei valori non validi, ciao!");
+		return -1;
+	}
+	return 0;
+}
